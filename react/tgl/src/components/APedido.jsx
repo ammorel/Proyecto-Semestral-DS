c6b09a87eefc36f8be468ea8549987b73cc2be98
tpavez <t.pavez.guzman@gmail.com>
2022-09-26 17:51:30 -0300

    Router pedido

--- react/tgl/src/components/APedido.jsx
+++ react/tgl/src/components/APedido.jsx
@@ -1,18 +1,37 @@
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
+import { NavLink } from "react-router-dom"
 
 function APedido() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="container">
         <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Cuéntanos sobre el accesorio que quieres pedir</Form.Label>
             <Form.Control as="textarea" rows={3} placeholder="Accesorio a pedir"/>
-        </Form.Group>
-        <Button type="submit" style={{color: "#FFFFFF"}}>
-            Pedir
-        </Button>
+        </Form.Group>   
         </Form>
+        <Button type="submit" onClick={handleShow}>
+            Pedir
+        </Button> 
+        <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+          <Modal.Header>
+            <Modal.Title>Pedido confirmado</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Gracias por confiar y pedir el accesorio con nosotros, nos pondremos en contacto contigo lo más pronto posible.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="submit" to="/" as={NavLink}>Volver a la pantalla de Inicio</Button>
+          </Modal.Footer>
+        </Modal>
     </div>
   );
 }
