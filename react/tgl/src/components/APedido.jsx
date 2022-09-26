c75b461d191c26e09930e456d99bbea3f335454e
tpavez <t.pavez.guzman@gmail.com>
2022-09-26 20:02:08 -0300

    Router pedido

--- react/tgl/src/components/APedido.jsx
+++ react/tgl/src/components/APedido.jsx
@@ -13,10 +13,21 @@ function APedido() {
   return (
     <div className="container">
         <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Group className="mb-3" controlId="formPedido">
+          <Form.Label>Nombre Completo</Form.Label>
+          <Form.Control type="name" placeholder="Ingrese su nombre completo" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPedido">
+          <Form.Label>Correo Electrónico</Form.Label>
+          <Form.Control type="email" placeholder="Ingrese su correo electrónico" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPedido">
             <Form.Label>Cuéntanos sobre el accesorio que quieres pedir</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Accesorio a pedir"/>
-        </Form.Group>   
+            <Form.Control as="textarea" rows={3} placeholder="Descripción de accesorio a pedir"/>
+        </Form.Group>  
+        
         </Form>
         <Button type="submit" onClick={handleShow}>
             Pedir
@@ -29,7 +40,7 @@ function APedido() {
             Gracias por confiar y pedir el accesorio con nosotros, nos pondremos en contacto contigo lo más pronto posible.
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="submit" to="/" as={NavLink}>Volver a la pantalla de Inicio</Button>
+            <Button type="submit" to="/" as={NavLink}>Volver a la pantalla de Inicio</Button>
           </Modal.Footer>
         </Modal>
     </div>
