54922081edfec1dcf2c0f1147c317342b503c3e6
Tomás Pavez <t.pavez.guzman@gmail.com>
2022-10-14 20:33:48 -0300

    footer

--- react/tgl/src/App.js
+++ react/tgl/src/App.js
@@ -9,7 +9,7 @@ import Nosotros from './components/Nosotros';
 import APedido from './components/APedido';
 import Anillos from './components/Anillos';
 import Pulseras from './components/Pulseras';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -24,7 +24,7 @@ function App() {
             <Route path='/pulseras' element={<Pulseras />}/>
             <Route path='/cart' element={<Cart />}/>
           </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </div>
   );
 }
