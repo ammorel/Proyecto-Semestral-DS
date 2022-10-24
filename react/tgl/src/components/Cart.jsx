9aa7d918950756aec0b993c5b819d87caa0fe664
Amparo Morel <ammorel@alumnos.uai.cl>
2022-10-23 22:32:12 -0300

    cart item

--- react/tgl/src/components/Cart.jsx
+++ react/tgl/src/components/Cart.jsx
@@ -1,9 +1,21 @@
 import Paypal from './Paypal';
+import { Stack } from "react-bootstrap"
+import { useCarrito } from "./Carrito.tsx";
+import { CartItem } from "./CartItem.tsx";
 
 function Cart() {
+    const {cartItems} = useCarrito()
     return (
-        <div className='paypal-button-container d-flex justify-content-center'>
-            <Paypal />
+        <div>
+            <Stack gap={3}>
+                {cartItems.map(item => ( 
+                <CartItem key = {item.id} {...item} />
+                ))}
+            </Stack>
+            
+            <div className='paypal-button-container d-flex justify-content-center'>
+                <Paypal />
+            </div>
         </div>
     );
 }
---
+++ react/tgl/src/components/CartItem.tsx
@@ -0,0 +1,35 @@
+import { useCarrito } from "./Carrito.tsx";
+import storeAnillos from ".//data/anillos.json"
+import { Stack } from "react-bootstrap";
+
+type CartItemProps = {
+    id: number,
+    cantidad: number
+}
+
+export function CartItem({ id, quantity}: CartItemProps) {
+    const { removeFromCart } = useCarrito()
+    
+    const item = storeAnillos.map().find(i => i["data"].id === id)
+    if (item == null) return null
+
+    return (
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+            <img src={item.imagen} style={{ width:"125px", height:"75px", objectFit:"cover"}}/>
+        
+            <div className="me-auto">
+                <div>
+                    {item.nombre}{" "} 
+                    {cantidad > 1 && (
+                        <span className="text-muted" style={{ fontSize: ".65rem" }}>
+                            x{cantidad}
+                        </span>
+                    )}
+                </div>
+                <div className="text-muted" style={{ fontSize:".75rem"}}>
+                    {item.precio}
+                </div>
+            </div>
+        </Stack>
+    )
+}
