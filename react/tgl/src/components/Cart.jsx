682260f0acf0e7f653e0bb5d5e9cc47c33cbb318
tpavez <t.pavez.guzman@gmail.com>
2022-10-25 19:45:50 -0300

    json

--- react/tgl/src/components/Cart.jsx
+++ react/tgl/src/components/Cart.jsx
@@ -1,10 +1,15 @@
 import Paypal from './Paypal';
+import Json from './Json';
 
 function Cart() {
     return (
+        <>
+        <Json/>
+
         <div className='paypal-button-container d-flex justify-content-center'>
             <Paypal />
         </div>
+        </>
     );
 }
 
